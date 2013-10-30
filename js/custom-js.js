fa152173fed58b16b60937137f701c8defa5df95
LucasRowe <lucasrowepublic@gmail.com>
2013-10-30 18:46:47 -0400

    First pass of Buzzwords.html added, including carousel.

--- js/custom-js.js
+++ js/custom-js.js
@@ -1,1 +1,9 @@
+// Highlight the active link in the navbar
 $('.nav > li > a[href="' + document.location.pathname + '"]').parent().addClass('active');
+
+// Off-Canvas (Toggles the side-bar for Buzzwords page)
+$(document).ready(function() {
+  $('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
+});
