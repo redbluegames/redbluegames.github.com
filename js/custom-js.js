8f5241f16a233111084611f3ad1e6e625afe0543
LucasRowe <lucasrowepublic@gmail.com>
2013-11-06 01:24:02 -0500

    Footer images now have a cute easter-egg flip on hover over.

--- js/custom-js.js
+++ js/custom-js.js
@@ -7,3 +7,12 @@ $(document).ready(function() {
     $('.row-offcanvas').toggleClass('active');
   });
 });
+
+
+// Handle flipping the little imagess in the footer
+$(".flipper").mouseenter(function() {
+  $(this).transition({
+    perspective: '100px',
+    rotateY: '180deg'
+  });
+});
